977ce825120f694e82c3961e49ee415da17ca0c4
Brandonn Guevara <jeunexavi256@live.com>
2024-04-29 21:33:58 -0400

    Added: Difficulty buttons to Endgame Screen

--- app/EndGame.tsx
+++ app/EndGame.tsx
@@ -16,8 +16,17 @@ export function EndGameAlert({score, startTimer} : {score : Number, startTimer: 
         </>
       </AlertDescription>
     </Alert><div className="timerButtonsContainer">
-        <Button variant="outline-success" id="timerButtons" className="px-4 py-2 mx-3" onClick={() => startTimer(0)}>
+        {/* <Button variant="outline-success" id="timerButtons" className="px-4 py-2 mx-3" onClick={() => startTimer(0)}>
           Start Game
+        </Button> */}
+        <Button variant="outline-success" id="timerButtons" className="px-4 py-2 mx-3" onClick={() => startTimer(0)}>
+          Beginner
+        </Button>
+        <Button variant="outline-info" id="timerButtons" className="px-4 py-2 mx-3" onClick={() => startTimer(1)}>
+          Easy
+        </Button>
+        <Button variant="outline-primary" id="timerButtons" className="px-4 py-2 mx-3" onClick={() => startTimer(2)}>
+          Normal
         </Button>
       </div></>
   )
